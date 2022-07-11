495acaf1582f1c2fa9ebefe39468e94598b03a35
PatrickHolderness <104070642+PatrickHolderness@users.noreply.github.com>
2022-07-11 18:06:55 +0200

    imported MovieCard, created movie array

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,6 +1,18 @@
-import React from 'react' ;
+import React from 'react';
+import { MovieCard } from '../movie-card/movie-card';
 
 export class MainView extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+            movies: [
+                { _id: 1, Title: 'Inception', Description: 'desc1...', ImagePath: '...' },
+                { _id: 2, Title: 'The Shawshank Redemption', Description: 'desc2...', ImagePath: '...' },
+                { _id: 3, Title: 'Gladiator', Description: 'desc3...', ImagePath: '...' }
+            ]
+        }
+    }
 
     render() {
         return (
@@ -12,3 +24,5 @@ export class MainView extends React.Component {
         );
     }
 }
+
+export default MainView;
