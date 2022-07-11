d4c264019a6139b0c14da04006af1fbe9db95774
PatrickHolderness <104070642+PatrickHolderness@users.noreply.github.com>
2022-07-11 15:05:02 +0200

    created main-view.jsx

---
+++ src/components/main-view/main-view.jsx
@@ -0,0 +1,14 @@
+import React from 'react' ;
+
+export class MainView extends React.Component {
+
+    render() {
+        return (
+            <div classNamre="main-view">
+                <div>Inception</div>
+                <div>The Shawshank Redemption</div>
+                <div>Gladiator</div>
+            </div>
+        );
+    }
+}
