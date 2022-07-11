a125dea182790e30ab0a83dad4ffbf6129a4fb92
PatrickHolderness <104070642+PatrickHolderness@users.noreply.github.com>
2022-07-11 15:30:28 +0200

    corrected spelling

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -4,7 +4,7 @@ export class MainView extends React.Component {
 
     render() {
         return (
-            <div classNamre="main-view">
+            <div className="main-view">
                 <div>Inception</div>
                 <div>The Shawshank Redemption</div>
                 <div>Gladiator</div>
