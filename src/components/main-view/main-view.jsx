376e079cc9985c960ebb3848550d68859b0f3c7d
PatrickHolderness <104070642+PatrickHolderness@users.noreply.github.com>
2022-07-15 15:52:02 +0200

    imported axios

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,27 +1,31 @@
 import React from 'react';
+import axios from 'axios';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 export class MainView extends React.Component {
-    
-  constructor() {
-        super();
-        this.state = {
-            movies: [
-                { 
-                _id: 1, Title: 'The Shawshank Redemption', Description: 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.', Genre:"Drama", Director:"Frank Darabont",  ImagePath: 'https://m.media-amazon.com/images/I/51P9fL8JeTL._AC_.jpg'
-                },
-                {
-                   _id: 2, Title: 'Inception', Description: 'A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O., but his tragic past may doom the project and his team to disaster.', Genre:"Action", Director:"Christopher Nolan", ImagePath: 'https://www.imdb.com/title/tt1375666/mediaviewer/rm3426651392/?ref_=tt_ov_i'},
-                {
-                    _id: 3, Title: '12 Angry Men', Description: 'The jury in a New York City murder trial is frustrated by a single member whose skeptical caution forces them to more carefully consider the evidence before jumping to a hasty verdict.', ImagePath: 'https://m.media-amazon.com/images/M/MV5BMWU4N2FjNzYtNTVkNC00NzQ0LTg0MjAtYTJlMjFhNGUxZDFmXkEyXkFqcGdeQXVyNjc1NTYyMjg@._V1_UX67_CR0,0,67,98_AL_.jpg',
-                },
-            ],
-            selectedMovie: null
-        }
-    }
-    setSelectedMovie(newSelectedMovie) {
+constructor(){
+    super();
+    this.state = {
+        movies: [],
+        selectedMovie: null
+    };
+}
+
+componentDidMount(){
+    axios.get('https://movie-info-online.herokuapp.com/movies')
+    .then(response => {
         this.setState({
-            selectedMovie: newSelectedMovie
+            movies: response.data
+        });
+    })
+    .catch(error => {
+        console.log(error);
+    });
+}
+
+setSelectedMovie(newSelectedMovie) {
+    this.setState({
+      selectedMovie: newSelectedMovie
         });
     }
     render() {
